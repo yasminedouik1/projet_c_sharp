77250edefb69587c50823b88ecb44ab90a7a952d
yasmine.douik <yasmine.douik@polytechnicien.tn>
2026-05-12 19:52:58 +0100

    login admin works

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using ProjectManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProjectManager.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<IdentityUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
@@ -14,6 +16,8 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+                base.OnModelCreating(modelBuilder);
+
         // Clé composite pour la table de jonction
         modelBuilder.Entity<ProjectMember>()
             .HasKey(pm => new { pm.ProjectId, pm.MemberId });
