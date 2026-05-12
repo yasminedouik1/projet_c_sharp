3b231f3ce013817cd99d9faefb1edf40a160d519
ghadaPoly <ghada.benmansour@polytechnicien.tn>
2026-05-12 16:13:59 +0200

    Ghada : partie 2 - getProjectsByMember

--- Services/IMyProjectsService.cs
+++ Services/IMyProjectsService.cs
@@ -9,4 +9,6 @@ public interface IMyProjectsService
     Task<List<Project>> GetProjectsByMemberAsync(int memberId);
     Task<List<ProjectTask>> GetTasksByMemberAndProjectAsync(int memberId, int projectId);
     Task UpdateTaskStatusAsync(int taskId, Models.TaskStatus newStatus);
+        Task<int> GetProjectMemberCountAsync(int memberId);
+
 }
